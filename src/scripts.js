d39a273b21dd6d52a2d7112c31d0cc15eecc925a
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-08-26 15:48:25 -0600

    Update userInfoUpdate to include step goal vs global goal average

--- src/scripts.js
+++ src/scripts.js
@@ -9,11 +9,11 @@ window.addEventListener('load', userInfoUpdate);
 function userInfoUpdate() {
   userNameNav.innerHTML = `<p>Welcome ${user.getFirstName()}!</p>`
   userInfoCard.innerHTML = `<div class="container">
-    <h4><b>${user.getFirstName()}</b></h4>
+    <h4><b>${user.getFirstName()}'s info</b></h4>
     <p>${user.address}</p>
     <p>${user.email}</p>
-    <p>${user.strideLength}</p>
-    <p>${user.dailyStepGoal}</p>
+    <p>Stride length: ${user.strideLength}</p>
+    <p>Your goal is: ${user.dailyStepGoal}</p>
+    <p>Global average goal: ${userRepository.getAverageStepGoals()}</p>
   </div>`
-  
 }
