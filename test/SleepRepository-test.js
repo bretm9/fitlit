517d6d935024bf431e040113fad1de82f7587212
Bret Merritt <merrittbret9@gmail.com>
2020-08-30 17:55:32 -0600

    Adjust data in test to work as expected

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -221,32 +221,32 @@ describe('SleepRepository', () => {
           userID: 1,
           date: '2019/06/15',
           hoursSlept: 6.1,
-          sleepQuality: 2.2
+          sleepQuality: 4.2
         },
         {
           userID: 1,
           date: '2019/06/16',
           hoursSlept: 4.1,
-          sleepQuality: 3.8
+          sleepQuality: 4.8
         },
-        { userID: 1, date: '2019/06/17', hoursSlept: 8, sleepQuality: 2.6 },
+        { userID: 1, date: '2019/06/17', hoursSlept: 8, sleepQuality: 4.6 },
         {
           userID: 1,
           date: '2019/06/18',
           hoursSlept: 10.4,
-          sleepQuality: 3.1
+          sleepQuality: 4.1
         },
         {
           userID: 1,
           date: '2019/06/19',
           hoursSlept: 10.7,
-          sleepQuality: 1.2
+          sleepQuality: 4.2
         },
         {
           userID: 1,
           date: '2019/06/20',
           hoursSlept: 9.3,
-          sleepQuality: 1.2
+          sleepQuality: 4.2
         },
         {
           userID: 1,
@@ -268,14 +268,14 @@ describe('SleepRepository', () => {
           userID: 2,
           date: '2019/06/16',
           hoursSlept: 4.1,
-          sleepQuality: 3.8
+          sleepQuality: 1.8
         },
         { userID: 2, date: '2019/06/17', hoursSlept: 8, sleepQuality: 2.8 },
         {
           userID: 2,
           date: '2019/06/18',
           hoursSlept: 10.4,
-          sleepQuality: 3.4
+          sleepQuality: 1.4
         },
         {
           userID: 2,
@@ -293,15 +293,15 @@ describe('SleepRepository', () => {
           userID: 2,
           date: '2019/06/21',
           hoursSlept: 7.8,
-          sleepQuality: 3.2
+          sleepQuality: 2.2
         }
       ]
     }
+
     sleepRepository = new SleepRepository(sleepData);
 
     sleepUser1 = new Sleep(1, sleepUserData1)
     sleepUser2 = new Sleep(2, sleepUserData2)
-
   });
  
   it('should be a function', () => {
@@ -318,7 +318,7 @@ describe('SleepRepository', () => {
  
   it('should be able to return average sleep quality for all users', () => { 
     let averageSleepQuality = sleepRepository.getAverageSleepQuality();
-    expect(averageSleepQuality).to.equal(2.7);
+    expect(averageSleepQuality).to.equal(3.2);
   });
 
   it('should be able to organize data by user', () => { 
@@ -332,10 +332,17 @@ describe('SleepRepository', () => {
     expect(sleepRepository.users).to.deep.equal([sleepUser1, sleepUser2]);
   });
 
-  it.only('should be able to return users with sleep quality above 3 based on a week', () => { 
+  it('should be able to return users with sleep quality above 3 based on a week', () => { 
     let organizedData = sleepRepository.getDataOrganizedByUser()
     sleepRepository.generateSleepObjects(organizedData);
-    let topThreeUsers = sleepRepository.getTopSleepUsers("2019/06/22");
+    let topThreeUsers = sleepRepository.getTopSleepUsers("2019/06/22", "sleepQuality");
+    expect(topThreeUsers).to.deep.equal([sleepUser1]);
+  });
+
+  it('should be able to return users with sleep quality above 3 based on a week', () => { 
+    let organizedData = sleepRepository.getDataOrganizedByUser()
+    sleepRepository.generateSleepObjects(organizedData);
+    let topThreeUsers = sleepRepository.getTopSleepUsers("2019/06/22", "sleepQuality");
     expect(topThreeUsers).to.deep.equal([sleepUser1]);
   });
 });
